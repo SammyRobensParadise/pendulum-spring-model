ad5c6dbe6012f7dd6f8ec38f28570b356cc6ee66
AlexHJC <58032512+AlexHJC@users.noreply.github.com>
2021-07-25 22:02:11 -0700

    Update twographattempt.py
    added comments

--- twographattempt.py
+++ twographattempt.py
@@ -1,26 +1,25 @@
-# importing the required modules
 import matplotlib.pyplot as plt
 import numpy as np
   
-# setting the x - coordinates
+# setting the x coordinates
 x = np.arange(0, 6*(np.pi), 0.01)
 
 
-# setting the corresponding y1 - coordinates
+# setting the y1 coordinates
 y1 = (np.sin(4*(np.pi)*x))*(np.cos(x))
   
-# potting the points
+# plot the points
 plt.plot(x, y1, label = "pendulum 1")
 
 
-# setting the corresponding y1 - coordinates
+# setting the y1 coordinates
 y2 = (np.sin(4*(np.pi)*x))*(np.cos(x + (np.pi / 2)))
   
-# potting the points
+# plot the points
 plt.plot(x, y2, label = "pendulum 2")
   
-# function to show legend
+# show legend
 plt.legend()
 
-# function to show the plot
+# show plot
 plt.show()
