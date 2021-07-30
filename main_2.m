fc1d5e951a1c9594f8e7bb5a38e61a85144f088c
Joon Park <71942561+joonprk@users.noreply.github.com>
2021-07-29 22:40:00 -0400

    Separated L into L_1 and L_2

--- main_2.m
+++ main_2.m
@@ -3,7 +3,8 @@ clc; clear;
 % Parameters 
 m_1=0.288;    
 m_2=0.288;
-L=0.2032; % Pendulum arm length 
+L_1=0.2032; 
+L_2=0.1832;
 d=0.2667; % Initial distance between two pendulums
 
 % Initial conditions: 
@@ -14,7 +15,7 @@ d=0.2667; % Initial distance between two pendulums
 % y1 = 2cm (0.02m)
 % y2 = 5mm (0.005m)
 % alpha = 5deg (0.0873rad)
-[t,y]=ode45(@(t,y) pendulum_2(t,y,m_1,m_2,L,d),[0 10],[0;0;0;0.524;0.349;0.05;0.03;0.02;0.005;0.0873]);
+[t,y]=ode45(@(t,y) pendulum_2(t,y,m_1,m_2,L_1,L_2,d),[0 10],[0;0;0;0.524;0.349;0.05;0.03;0.02;0.005;0.0873]);
 
 % Graph in separate figures: 
 % th1=y(:,4); 
